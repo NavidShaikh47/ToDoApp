77fae15d0aa39f46fd04ac9debde6b783ecfc879
Navid Shaikh <navidhsiakh47@gmail.com>
2022-08-20 09:41:26 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// index.js has been made now npm init so launch server 
+// after npm init make directories routes models controllers views and config
+// routes will store all the paths and fuctn called in routes will be stored in controller 
+// models will store schemas , config will store mongoose config and views will be our view engine pages
+// now fire up our express server,  npm install express and check package.jason dependancies
